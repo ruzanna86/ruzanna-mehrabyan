dcf819e3122082566945a35adb646fb7b07e1e6a
Ruzanna <ruzanna.mehrab@gmail.com>
2019-08-09 21:57:29 +0400

    Add existing files

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	"use strict";
+    
+        /*==================================
+* Author        : "Ruzanna"
+* Template Name : CV HTML, CSS Template
+* Version       : 1.0
+==================================== */
+
+
+
+/*=========== TABLE OF CONTENTS ===========
+1. Scroll To Top 
+2. Smooth Scroll spy
+3. Progress-bar
+4. owl carousel
+5. welcome animation support
+======================================*/
+
+	// 3. Progress-bar
+
+});	
+	
